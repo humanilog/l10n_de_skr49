cb21159585b7d7c78ca9cf5be57fd37331ec64cd
Stefan Becker <ich@funbaker.de>
2016-03-16 10:01:45 +0100

    initial commit

---
+++ __openerp__.py
@@ -0,0 +1,22 @@
+{
+  'name': 'Deutschland SKR04 HLO - Accounting',
+  'version': '1.0',
+  'author': 'HLO',
+  'website': 'https://humanilog.org',
+  'category': 'Localization/Account Charts',
+  'description': """
+Dieses Modul beinhaltet einen deutschen Kontenrahmen für die HLO basierend auf dem SKR49.
+==============================================================================
+
+German accounting chart and localization for the HLO.
+  """,
+  'depends': ['l10n_de'],
+  'demo': [],
+  'data': [
+    'account.account.type.csv',
+    'account_chart.xml',
+    'account_tax_fiscal_position.xml',
+    'account.chart.template.yml',
+  ],
+  'installable': True,
+}
