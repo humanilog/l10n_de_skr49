d81f7d6b1e5f085fd3476ee45453c0ab746a01dd
Franz Becker <franz.becker@itemis.de>
2018-10-24 22:40:00 +0200

    remove data tag and hlo branding

--- __manifest__.py
+++ __manifest__.py
@@ -1,5 +1,5 @@
 {
-  'name': 'Deutschland SKR49 HLO - Accounting',
+  'name': 'Deutschland SKR49 - Accounting',
   'version': '2.0',
   'author': 'humanilog',
   'website': 'https://humanilog.org',
@@ -12,6 +12,7 @@ German accounting chart and localization for the SKR49.
   """,
   'depends': ['l10n_de'],
   'data': [
+    'data/account_account_tags_data.xml',
     'data/account_chart.xml',
     'data/account_tax_fiscal_position.xml',
     'data/account.chart.template.yml',
