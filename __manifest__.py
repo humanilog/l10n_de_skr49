76fecb4f4a179a06f39ba880bd82b88b37219fe4
HaGuesto <guertneh@student.hu-berlin.de>
2019-04-24 14:38:11 +0200

    10.0 migrate to new (#1)
    * update xml using our new tool
    
    * delete now unused account types
    
    * remove data tag and hlo branding
    
    * fix xml
    
    * change category so that this module is found as localization
    
    * update xml
    
    see https://github.com/humanilog/odoo-account-chart-tool/tree/v0.1
    
    * update xml
    
    see https://github.com/humanilog/odoo-account-chart-tool/tree/v0.2
    
    * Deleting empty spaces in field values.

--- __manifest__.py
+++ __manifest__.py
@@ -1,18 +1,18 @@
 {
-  'name': 'Deutschland SKR49 HLO - Accounting',
+  'name': 'Deutschland SKR49 - Accounting',
   'version': '2.0',
-  'author': 'HLO',
+  'author': 'humanilog',
   'website': 'https://humanilog.org',
-  'category': 'Localization/Account Charts',
+  'category': 'Localization',
   'description': """
-Dieses Modul beinhaltet einen deutschen Kontenrahmen für die HLO basierend auf dem SKR49.
+Dieses Modul beinhaltet einen deutschen Kontenrahmen basierend auf dem SKR49.
 =========================================================================================
 
-German accounting chart and localization for the HLO.
+German accounting chart and localization for the SKR49.
   """,
   'depends': ['l10n_de'],
   'data': [
-    'data/account.account.type.csv',
+    'data/account_account_tags_data.xml',
     'data/account_chart.xml',
     'data/account_tax_fiscal_position.xml',
     'data/account.chart.template.yml',
