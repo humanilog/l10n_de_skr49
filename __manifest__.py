90399da668af9d062803a91a31f2cee3a516ba46
HaGuesto <h.guertner@humanilog.org>
2019-06-03 13:05:23 +0200

    Migrate to 11.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,21 +1,21 @@
 {
-  'name': 'Deutschland SKR49 - Accounting',
-  'version': '2.0',
-  'author': 'humanilog',
-  'website': 'https://humanilog.org',
-  'category': 'Localization',
-  'description': """
+    'name': 'Deutschland SKR49 - Accounting',
+    'version': '11.0.1.0.0',
+    'author': 'humanilog',
+    'website': 'https://humanilog.org',
+    'category': 'Localization',
+    'description': """
 Dieses Modul beinhaltet einen deutschen Kontenrahmen basierend auf dem SKR49.
 =========================================================================================
 
 German accounting chart and localization for the SKR49.
   """,
-  'depends': ['l10n_de'],
-  'data': [
-    'data/account_account_tags_data.xml',
-    'data/account_chart.xml',
-    'data/account_tax_fiscal_position.xml',
-    'data/account.chart.template.yml',
-  ],
-  'installable': True,
+    'depends': ['l10n_de'],
+    'data': [
+        'data/account_account_tags_data.xml',
+        'data/account_chart.xml',
+        'data/account_tax_fiscal_position.xml',
+        'data/account.chart.template.yml',
+    ],
+    'installable': True,
 }
